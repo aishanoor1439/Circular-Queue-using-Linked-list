eb84994b513089a1475547a6cfe4a836b4f53560
aishanoor1439 <aisha.noor1439@gmail.com>
2026-01-19 19:30:43 +0500

    First Commit
    added project files

---
+++ CircularQueueUsingLinkedList/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularQueueUsingLinkedList
+{
+    internal class Program
+    {
+        class Node
+        {
+            public int Data;
+            public Node Next;
+            public Node(int data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+
+        class CircularQueue
+        {
+            private Node front;
+            private Node rear;
+
+            public CircularQueue()
+            {
+                front = rear = null;
+            }
+
+            public bool IsEmpty()
+            {
+                return front == null && rear == null;
+            }
+
+            public void Enqueue(int x)
+            {
+                Node newNode = new Node(x);
+                if (IsEmpty())
+                {
+                    front = rear = newNode;
+                    rear.Next = front;
+                }
+                else
+                {
+                    rear.Next = newNode;
+                    rear = newNode;
+                    rear.Next = front;
+                }
+                Console.WriteLine($"{x} enqueued successfully.");
+            }
+
+            public void Dequeue()
+            {
+                if (IsEmpty())
+                {
+                    Console.WriteLine("Queue is empty");
+                    return;
+                }
+                if (front == rear)
+                {
+                    Console.WriteLine($"Dequeued element: {front.Data}");
+                    front = rear = null;
+                }
+                else
+                {
+                    Console.WriteLine($"Dequeued element: {front.Data}");
+                    front = front.Next;
+                    rear.Next = front;
+                }
+            }
+
+            public void Peek()
+            {
+                if (IsEmpty())
+                {
+                    Console.WriteLine("Queue is empty");
+                    return;
+                }
+                Console.WriteLine($"Front element: {front.Data}");
+            }
+
+            public void Display()
+            {
+                if (IsEmpty())
+                {
+                    Console.WriteLine("Queue is empty");
+                    return;
+                }
+                Console.Write("Queue elements: ");
+                Node temp = front;
+                do
+                {
+                    Console.Write(temp.Data + " ");
+                    temp = temp.Next;
+                } while (temp != front);
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CircularQueue q = new CircularQueue();
+
+            q.Enqueue(10);
+            q.Enqueue(20);
+            q.Enqueue(30);
+            q.Enqueue(40);
+            q.Enqueue(50);
+
+            q.Display();
+            q.Peek();
+
+            q.Dequeue();
+            q.Dequeue();
+
+            q.Display();
+
+            q.Enqueue(60);
+            q.Enqueue(70);
+
+            q.Display();
+            q.Peek();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CircularQueueUsingLinkedList/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CircularQueueUsingLinkedList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CircularQueueUsingLinkedList")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c5eb91f-04c9-4be1-8ee5-53182191e79f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
